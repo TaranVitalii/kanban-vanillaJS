c3b00a4cf88dba12e3add9f667a7004f72bc7d7a
ikn0wQQ <kn0wnOutCast@gmail.com>
2019-06-07 06:35:47 +0300

    my version

---
+++ src/Drag'n'Drop.js
@@ -0,0 +1,27 @@
+import {updateColumnId} from './cards1' ;
+export function dragStart(event){
+let idColumnBefore = +event.currentTarget.closest('[data-column]').getAttribute('data-column');
+let targetIdCard = +event.target.getAttribute('data-card');
+let object = JSON.stringify({
+        idColumn:idColumnBefore,
+        targetId:targetIdCard
+});
+    event.dataTransfer.setData("object", object);
+		
+}
+export function dragOver(event){
+	event.preventDefault();
+}
+
+export async function onDrop(event){
+	event.preventDefault();
+			
+		let targetColumn= +event.target.getAttribute('data-column');
+		let data = JSON.parse(event.dataTransfer.getData("object"));
+				
+		if(data.idColumn != targetColumn){
+			let cardFromSetData = document.querySelector(`[data-card="${data.targetId}`);
+			this.append(cardFromSetData);
+			let response = await updateColumnId(data.targetId, targetColumn);
+	  		}
+		}
